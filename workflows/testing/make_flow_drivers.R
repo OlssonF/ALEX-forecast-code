4988d2aa4f28a1808e165844acc990ece69842eb
Freya E Olsson <freyao@tc013.cm.cluster>
2024-08-27 16:45:29 -0400

    improved config

--- workflows/testing/make_flow_drivers.R
+++ workflows/testing/make_flow_drivers.R
@@ -8,7 +8,10 @@ hist_interp_inflow <- interpolate_targets(targets = 'ALEX-targets-inflow.csv',
                                          site_id = 'ALEX', 
                                          variables = c('FLOW', 'SALT', 'TEMP'),
                                          depth = F, 
-                                         method = 'linear')
+                                         method = 'linear')  |> 
+  mutate(flow_number = 1, 
+         parameter = 1) |> 
+  rename(prediction = observation)
 
 # Write the interpolated data as the historal file
 arrow::write_dataset(hist_interp_inflow,
@@ -17,12 +20,13 @@ arrow::write_dataset(hist_interp_inflow,
 # generate a simple "forecast" that has ensemble members
 forecast_date <- config$run_config$forecast_start_datetime
 future_inflow <- hist_interp_inflow |> 
-  filter(datetime > forecast_date,
+  filter(datetime >= as_datetime(forecast_date),
          datetime <= as_date(forecast_date) + config$run_config$forecast_horizon) |> 
-  reframe(prediction = rnorm(10, mean = observation, sd = 1), 
+  reframe(prediction = rnorm(10, mean = prediction, sd = 1), 
           parameter = 1:10,
-          .by = c(site_id, datetime, variable)) |> 
-  mutate(reference_datetime = as_date(forecast_date))
+          .by = c(site_id, datetime, variable, flow_number)) |> 
+  mutate(reference_datetime = as_date(forecast_date),
+         prediction = ifelse(prediction <0, 0, prediction))
 
 arrow::write_dataset(future_inflow,
                      'drivers/inflow/future/historical_interp/',
@@ -37,7 +41,11 @@ hist_interp_outflow <- interpolate_targets(targets = 'ALEX-targets-outflow.csv',
                                           site_id = 'ALEX', 
                                           variables = c('FLOW', 'SALT', 'TEMP'),
                                           depth = F, 
-                                          method = 'linear')
+                                          method = 'linear') |> 
+  mutate(flow_number = 1, 
+         parameter = 1) |> 
+  rename(prediction = observation)
+
 
 # Write the interpolated data as the historal file
 arrow::write_dataset(hist_interp_inflow,
@@ -45,12 +53,13 @@ arrow::write_dataset(hist_interp_inflow,
 
 # generate a simple "forecast" that has ensemble members
 future_outflow <- hist_interp_outflow |> 
-  filter(datetime > forecast_date,
+  filter(datetime >= as_datetime(forecast_date),
          datetime <= as_date(forecast_date) + config$run_config$forecast_horizon) |> 
-  reframe(prediction = rnorm(10, mean = observation, sd = 1), 
+  reframe(prediction = rnorm(10, mean = prediction, sd = 1), 
           parameter = 1:10,
-          .by = c(site_id, datetime, variable)) |> 
-  mutate(reference_datetime = as_date(forecast_date))
+          .by = c(site_id, datetime, variable, flow_number)) |> 
+  mutate(reference_datetime = as_date(forecast_date),
+         prediction = ifelse(prediction <0, 0, prediction))
 
 arrow::write_dataset(future_outflow,
                      'drivers/outflow/future/historical_interp/',
