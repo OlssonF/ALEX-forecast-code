77c86ca2344016b4e0be0dd5837e237b1287abc9
addelany <addelany@gmail.com>
2025-02-20 10:38:01 -0600

    write met correctly

--- workflows/glm_flare_v3/generate_targets.R
+++ workflows/glm_flare_v3/generate_targets.R
@@ -144,8 +144,9 @@ wind_dir_obs <- read_csv('wind_direction_obs.csv', skip=1) |>
 
 cleaned_wind_file <- file.path(config$file_path$qaqc_data_directory, paste0(config$location$site_id, "-targets-met.csv"))
 
-wind_obs <- dplyr::bind_rows(wind_velocity_obs, wind_dir_obs) |> 
-  write_csv(cleaned_wind_file)
+met_obs <- dplyr::bind_rows(wind_velocity_obs, wind_dir_obs) 
+  
+write_csv(met_obs)
 
 # Individual barrage flows
 library(httr)
