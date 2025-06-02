6fad52d3c6e0da97dcdd21e4a7274e49f45eb620
Freya Olsson <freyao@vt.edu>
2025-06-02 19:53:21 +0000

    fix file path

--- workflows/scenario_reforecasts/download_forecast_scores.R
+++ workflows/scenario_reforecasts/download_forecast_scores.R
@@ -7,8 +7,8 @@ unzip(file.path(lake_directory,"scores.zip"))
 
 # Drivers
 download.file(url = "https://sandbox.zenodo.org/records/264303/files/drivers.zip?download=1",
-              destfile = file.path(lake_directory,"scores.zip"), method = "curl")
-unzip(file.path(lake_directory,"scores.zip"))
+              destfile = file.path(lake_directory,"drivers.zip"), method = "curl")
+unzip(file.path(lake_directory,"drivers.zip"))
 
 # Forecasts
 download.file(url = "https://sandbox.zenodo.org/records/264303/files/forecasts.zip?download=1",
