3bc1652238f4e3907c2edae64ce2754262df7994
Freya Olsson <freyao@vt.edu>
2025-06-02 20:19:06 +0000

    try archive again

--- workflows/scenario_reforecasts/download_forecast_scores.R
+++ workflows/scenario_reforecasts/download_forecast_scores.R
@@ -1,22 +1,22 @@
 lake_directory <- here::here()
 
 # Scores
-download.file(url = "https://sandbox.zenodo.org/records/264303/files/scores.zip?download=1",
+download.file(url = "https://sandbox.zenodo.org/records/264318/files/scores.zip?download=1",
               destfile = file.path(lake_directory,"scores.zip"), method = "curl")
 unzip(file.path(lake_directory,"scores.zip"))
 
 # Drivers
-download.file(url = "https://sandbox.zenodo.org/records/264303/files/drivers.zip?download=1",
+download.file(url = "https://sandbox.zenodo.org/records/264318/files/drivers.zip?download=1",
               destfile = file.path(lake_directory,"drivers.zip"), method = "curl")
 unzip(file.path(lake_directory,"drivers.zip"))
 
 # Forecasts
-download.file(url = "https://sandbox.zenodo.org/records/264303/files/forecasts.zip?download=1",
+download.file(url = "https://sandbox.zenodo.org/records/264318/files/forecasts.zip?download=1",
               destfile = file.path(lake_directory,"forecasts.zip"), method = "curl")
 unzip(file.path(lake_directory,"forecasts.zip"))
 
 # Targets (observations)
-download.file(url = "https://sandbox.zenodo.org/records/264303/files/targets.zip?download=1",
+download.file(url = "https://sandbox.zenodo.org/records/264318/files/targets.zip?download=1",
               destfile = file.path(lake_directory,"targets.zip"), method = "curl")
 unzip(file.path(lake_directory,"targets.zip"))
 
