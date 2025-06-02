9d906825a687ceb6347b4481e271f95c5657d6ae
Freya Olsson <freyao@vt.edu>
2025-06-02 20:39:29 +0000

    add missing package

--- workflows/scenario_reforecasts/install_packages.R
+++ workflows/scenario_reforecasts/install_packages.R
@@ -1,5 +1,5 @@
 # install packages
-install.packages(c("remotes", "tidyverse", "lubridate", "arrow", "fable", "fabletools", "imputeTS", "tidymodels", "tsibble", "ggpubr", "ggrepel"))
+install.packages(c("remotes", "tidyverse", "lubridate", "arrow", "fable", "fabletools", "imputeTS", "tidymodels", "tsibble", "ggpubr", "ggrepel", "gsw"))
 install.packages(c("here", "aws.s3"))
 
 remotes::install_github("FLARE-forecast/FLAREr", ref = "v3.0.3")
