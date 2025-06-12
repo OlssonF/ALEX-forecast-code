065a9db292cc05ea7749fde21617fc4b369ba327
Freya Olsson <47249151+OlssonF@users.noreply.github.com>
2025-06-12 10:06:35 -0400

    Update download_forecast_scores with zenodo link

--- workflows/scenario_reforecasts/download_forecast_scores.R
+++ workflows/scenario_reforecasts/download_forecast_scores.R
@@ -1,21 +1,21 @@
 lake_directory <- here::here()
 
 # Scores
-download.file(url = "https://sandbox.zenodo.org/records/267993/files/scores.zip?download=1",
+download.file(url = "https://zenodo.org/records/15649265/files/scores.zip?download=1",
               destfile = file.path(lake_directory,"scores.zip"), method = "curl")
 unzip(file.path(lake_directory,"scores.zip"))
 
 # Drivers
-download.file(url = "https://sandbox.zenodo.org/records/267993/files/drivers.zip?download=1",
+download.file(url = "https://zenodo.org/records/15649265/files/drivers.zip?download=1",
               destfile = file.path(lake_directory,"drivers.zip"), method = "curl")
 unzip(file.path(lake_directory,"drivers.zip"))
 
 # Forecasts
-download.file(url = "https://sandbox.zenodo.org/records/267993/files/forecasts.zip?download=1",
+download.file(url = "https://zenodo.org/records/15649265/files/forecasts.zip?download=1",
               destfile = file.path(lake_directory,"forecasts.zip"), method = "curl")
 unzip(file.path(lake_directory,"forecasts.zip"))
 
 # Targets (observations)
-download.file(url = "https://sandbox.zenodo.org/records/267993/files/targets.zip?download=1",
+download.file(url = "https://zenodo.org/records/15649265/files/targets.zip?download=1",
               destfile = file.path(lake_directory,"targets.zip"), method = "curl")
 unzip(file.path(lake_directory,"targets.zip"))
